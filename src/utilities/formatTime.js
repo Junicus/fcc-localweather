daed2243509b89a1f9b43d35adade3f4013f37a4
Nelson Segarra <junicus@outlook.com>
2017-08-05 03:00:24 -0400

    change time formatter

--- src/utilities/formatTime.js
+++ src/utilities/formatTime.js
@@ -1,19 +1,5 @@
 export function formatTime(time) {
-    let formatter = new Intl.DateTimeFormat(undefined, {
-        weekday: 'long',
-        year: 'numeric',
-        month: 'numeric',
-        day: 'numeric',
-        hour: 'numeric',
-        minute: 'numeric',
-        second: 'numeric',
-        hour12: true,
-        timeZone: 'UTC'
-    });
-    let hourPart = formatter.formatToParts(time).filter((part) => part.type === 'hour')[0];
-    let minutePart = formatter.formatToParts(time).filter((part) => part.type === 'minute')[0];
-    let dayPeriod = formatter.formatToParts(time).filter((part) => part.type === 'dayperiod')[0];
-
-    console.log(hourPart, minutePart, dayPeriod);
-    return `${hourPart['value']}:${minutePart['value']} ${dayPeriod['value']}`;
+    let options = { hour: 'numeric', minute: 'numeric' };
+    let dateTimeFormat = new Intl.DateTimeFormat(undefined, options);
+    return dateTimeFormat.format(time);
 }
